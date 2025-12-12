3fda0bfc9dd721a52f785a298322a398a1bdb979
Stephan Kulla <git.mail@kulla.me>
2025-12-12 13:16:30 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import '@picocss/pico/css/pico.min.css'
+import './App.css'
+
+export default function App() {
+  return (
+    <main className="p-10">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </main>
+  )
+}
