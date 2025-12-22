179f87b3540ff6c17f74d3267ffde3e45d0c08f4
Stephan Kulla <git.mail@kulla.me>
2025-12-22 20:26:04 +0100

    feat(schema): Add SchemaDef

--- src/schema.ts
+++ src/schema.ts
@@ -1,17 +1,26 @@
+import {Guard} from "./utils"
+
 declare const TypeInformation: unique symbol
 
-export interface SchemaType {
+interface SchemaDef {
   kind: string
-  name: string
-  [TypeInformation]?: {
-    FlatValue: unknown
-    JSONValue: unknown
-  }
+  FlatValue: unknown
+  JSONValue: unknown
 }
 
-export type FlatValue<S extends SchemaType> =
+export interface Schema<D extends SchemaDef = SchemaDef> {
+  kind: D['kind']
+  name: string
+  [TypeInformation]?: {
+    FlatValue: D['FlatValue']
+    JSONValue: D['JSONValue']
+  }
+  isFlatValue: Guard.Guard<D['FlatValue']>
+}
+
+export type FlatValue<S extends Schema> =
   NonNullable<S[typeof TypeInformation]>['FlatValue']
 
-export type JSONValue<S extends SchemaType> =
+export type JSONValue<S extends Schema> =
   NonNullable<S[typeof TypeInformation]>['JSONValue']
 
