e2dcd2a36f3716570de362bff3af0214b293be40
Stephan Kulla <git.mail@kulla.me>
2025-12-22 20:23:12 +0100

    feat(schema): Make type information optional

--- src/schema.ts
+++ src/schema.ts
@@ -3,15 +3,15 @@ declare const TypeInformation: unique symbol
 export interface SchemaType {
   kind: string
   name: string
-  [TypeInformation]: {
+  [TypeInformation]?: {
     FlatValue: unknown
     JSONValue: unknown
   }
 }
 
 export type FlatValue<S extends SchemaType> =
-  S[typeof TypeInformation]['FlatValue']
+  NonNullable<S[typeof TypeInformation]>['FlatValue']
 
 export type JSONValue<S extends SchemaType> =
-  S[typeof TypeInformation]['JSONValue']
+  NonNullable<S[typeof TypeInformation]>['JSONValue']
 
