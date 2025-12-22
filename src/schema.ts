5fe19778cc5a7bb3267e0645135861c9a6a1c9eb
Stephan Kulla <git.mail@kulla.me>
2025-12-22 20:47:31 +0100

    feat(schema): Add string and union schema factories

--- src/schema.ts
+++ src/schema.ts
@@ -1,3 +1,4 @@
+import { isKey, type Key } from './store/key'
 import { Guard } from './utils'
 
 declare const TypeInformation: unique symbol
@@ -26,13 +27,6 @@ export type JSONValue<S extends Schema> = NonNullable<
   S[typeof TypeInformation]
 >['JSONValue']
 
-interface BooleanSchema
-  extends Schema<{
-    kind: 'boolean'
-    FlatValue: boolean
-    JSONValue: boolean
-  }> {}
-
 function createSchemaGuard<S extends Schema>(kind: string): Guard.Guard<S> {
   return (value: unknown): value is S => {
     return (
@@ -44,9 +38,46 @@ function createSchemaGuard<S extends Schema>(kind: string): Guard.Guard<S> {
   }
 }
 
+interface BooleanSchema
+  extends Schema<{
+    kind: 'boolean'
+    FlatValue: boolean
+    JSONValue: boolean
+  }> {}
+
 export const boolean = {
   create(name: string): BooleanSchema {
     return { kind: 'boolean', name, isFlatValue: Guard.boolean }
   },
   is: createSchemaGuard<BooleanSchema>('boolean'),
+}
+
+interface StringSchema
+  extends Schema<{
+    kind: 'string'
+    FlatValue: string
+    JSONValue: string
+  }> {}
+
+export const string = {
+  create(name: string): StringSchema {
+    return { kind: 'string', name, isFlatValue: Guard.string }
+  },
+  is: createSchemaGuard<StringSchema>('string'),
+}
+
+interface UnionSchema<S extends Schema[] = Schema[]>
+  extends Schema<{
+    kind: 'union'
+    FlatValue: Key
+    JSONValue: JSONValue<S[number]>
+  }> {
+  options: S
+}
+
+export const union = {
+  create<S extends Schema[]>(name: string, options: S): UnionSchema<S> {
+    return { kind: 'union', name, options, isFlatValue: isKey }
+  },
+  is: createSchemaGuard<UnionSchema>('union'),
 }
--- src/store/key.ts
+++ src/store/key.ts
@@ -1,3 +1,7 @@
 import type { Branded } from '../utils'
 
 export type Key = Branded<string, 'Key'>
+
+export function isKey(value: unknown): value is Key {
+  return typeof value === 'string'
+}
