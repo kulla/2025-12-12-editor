07f15001d6bd8ebf4d7af742582df3f3132a73de
Stephan Kulla <git.mail@kulla.me>
2025-12-23 14:56:00 +0100

    feat(store): Add KeyGenerator and PrefixKeyGenerator

--- src/store/key.ts
+++ src/store/key.ts
@@ -4,3 +4,19 @@ import { type Guard, isString } from '../utils/guard'
 export type Key = Branded<string, 'Key'>
 
 export const isKey = isString as Guard<Key>
+
+export interface KeyGenerator {
+  next(): Key
+}
+
+export class PrefixKeyGenerator implements KeyGenerator {
+  private counter = 0
+
+  constructor(private readonly prefix: string) {}
+
+  next(): Key {
+    this.counter += 1
+
+    return (this.prefix + this.counter) as Key
+  }
+}
