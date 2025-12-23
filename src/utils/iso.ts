93a52db171ba12205e33a56473e367b9c3ed08e1
Stephan Kulla <git.mail@kulla.me>
2025-12-23 14:32:22 +0100

    refactor(utils): Delete identityIso()

--- src/utils/iso.ts
+++ src/utils/iso.ts
@@ -2,10 +2,3 @@ export interface Iso<From, To> {
   to: (value: From) => To
   from: (value: To) => From
 }
-
-export function identityIso<T>(): Iso<T, T> {
-  return {
-    to: (value: T) => value,
-    from: (value: T) => value,
-  }
-}
