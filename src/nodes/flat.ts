2a991b7e92d943e4515751bc8bf70cdb6d0eb23b
Stephan Kulla <git.mail@kulla.me>
2025-12-23 23:46:25 +0100

    feat(flat-node): Add concrete type guards

--- src/nodes/flat.ts
+++ src/nodes/flat.ts
@@ -1,3 +1,14 @@
+import {
+  isArraySchema,
+  isLiteralSchema,
+  isObjectSchema,
+  isPrimitiveSchema,
+  isRichTextSchema,
+  isSingletonSchema,
+  isTruthValueSchema,
+  isUnionSchema,
+  isWrapperSchema,
+} from '../schema/kinds'
 import type { FlatValue, Schema } from '../schema/types'
 import type { Key } from '../store/key'
 import type { Guard } from '../utils/guard'
@@ -9,9 +20,16 @@ export interface FlatNode<S extends Schema = Schema> {
   value: FlatValue<S>
 }
 
-export function isFlatNodeKind<S extends Schema>(
-  schemaGuard: Guard<S>,
-  node: FlatNode,
-): node is FlatNode<S> {
-  return schemaGuard(node.schema)
+export const isTruthValueFlatNode = createFlatNodeGuard(isTruthValueSchema)
+export const isRichTextFlatNode = createFlatNodeGuard(isRichTextSchema)
+export const isLiteralFlatNode = createFlatNodeGuard(isLiteralSchema)
+export const isWrapperFlatNode = createFlatNodeGuard(isWrapperSchema)
+export const isUnionFlatNode = createFlatNodeGuard(isUnionSchema)
+export const isArrayFlatNode = createFlatNodeGuard(isArraySchema)
+export const isObjectFlatNode = createFlatNodeGuard(isObjectSchema)
+export const isPrimitiveFlatNode = createFlatNodeGuard(isPrimitiveSchema)
+export const isSingletonFlatNode = createFlatNodeGuard(isSingletonSchema)
+
+function createFlatNodeGuard<S extends Schema>(schemaGuard: Guard<S>) {
+  return (node: FlatNode): node is FlatNode<S> => schemaGuard(node.schema)
 }
