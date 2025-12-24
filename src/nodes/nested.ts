6c52087aee8f651bc46755b7f3aea7f107d9f415
Stephan Kulla <git.mail@kulla.me>
2025-12-24 01:04:46 +0100

    feat(nested-node): Add type guards

--- src/nodes/nested.ts
+++ src/nodes/nested.ts
@@ -1,6 +1,32 @@
+import {
+  isArraySchema,
+  isLiteralSchema,
+  isObjectSchema,
+  isPrimitiveSchema,
+  isRichTextSchema,
+  isSingletonSchema,
+  isTruthValueSchema,
+  isUnionSchema,
+  isWrapperSchema,
+} from '../schema/kinds'
 import type { JSONValue, Schema } from '../schema/types'
+import type { Guard } from '../utils/guard'
 
 export interface NestedNode<S extends Schema = Schema> {
   schema: S
   value: JSONValue<S>
+}
+
+export const isTruthValueNestedNode = createNestedNodeGuard(isTruthValueSchema)
+export const isRichTextNestedNode = createNestedNodeGuard(isRichTextSchema)
+export const isLiteralNestedNode = createNestedNodeGuard(isLiteralSchema)
+export const isWrapperNestedNode = createNestedNodeGuard(isWrapperSchema)
+export const isUnionNestedNode = createNestedNodeGuard(isUnionSchema)
+export const isArrayNestedNode = createNestedNodeGuard(isArraySchema)
+export const isObjectNestedNode = createNestedNodeGuard(isObjectSchema)
+export const isPrimitiveNestedNode = createNestedNodeGuard(isPrimitiveSchema)
+export const isSingletonNestedNode = createNestedNodeGuard(isSingletonSchema)
+
+function createNestedNodeGuard<S extends Schema>(schemaGuard: Guard<S>) {
+  return (node: NestedNode): node is NestedNode<S> => schemaGuard(node.schema)
 }
