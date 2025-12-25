5661689ad698f238dd3195e72e01f09b9b621f3a
Stephan Kulla <git.mail@kulla.me>
2025-12-25 13:22:56 +0100

    feat: store initial content

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import '@picocss/pico/css/pico.min.css'
 import './App.css'
 import { LoroDoc } from 'loro-crdt'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
+import { initialContent } from './content/initial-content'
+import { Root } from './content/types'
+import { storeRoot } from './operations/store'
 import type { Key } from './store/key'
 import { useEditorStore } from './store/use-editor-store'
 
 export default function App() {
-  const rootKey = "root" as Key
+  const rootKey = 'root' as Key
   const loroDoc = useRef(new LoroDoc()).current
   const { store } = useEditorStore(loroDoc)
 
   useEffect(() => {
     if (store.get(rootKey) != null) return
 
-    store.update(tx => {
-
+    store.update((tx) => {
+      storeRoot({
+        tx,
+        rootKey,
+        node: { schema: Root, value: initialContent },
+      })
     })
+  }, [rootKey, store])
 
   return (
     <main className="p-10">
