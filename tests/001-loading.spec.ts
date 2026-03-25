570e9429c644106886b53c0bd7a15b22060ebf70
Stephan Kulla <git.mail@kulla.me>
2026-03-25 13:30:39 +0100

    test: Format loading.spec.ts

--- tests/001-loading.spec.ts
+++ tests/001-loading.spec.ts
@@ -4,11 +4,8 @@ test('Editor prototype should load', async ({ page }) => {
   await page.goto('http://localhost:3000')
   await page.waitForSelector('.ProseMirror')
 
-  await expect(
-    page
-      .getByText(
-        'This is an example of educational content with various types of items.',
-      )
-      .first(),
-  ).toBeVisible()
+  const firstParagraphText =
+    'This is an example of educational content with various types of items.'
+
+  await expect(page.getByText(firstParagraphText).first()).toBeVisible()
 })
