7969544395db85bb2a5c377da3e879ce9ccf8c66
Stephan Kulla <git.mail@kulla.me>
2026-03-26 13:44:16 +0100

    docs: Comment line in loadPrototype()

--- tests/utils.ts
+++ tests/utils.ts
@@ -2,5 +2,7 @@ import type { Page } from 'playwright/test'
 
 export async function loadPrototype(page: Page) {
   await page.goto('http://localhost:3000')
+
+  // Wait for the editor to load
   await page.waitForSelector('.ProseMirror')
 }
