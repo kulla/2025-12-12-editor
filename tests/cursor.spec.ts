214dbc5e1fd9ef50ed3067844e8ad623e2df6022
Stephan Kulla <git.mail@kulla.me>
2026-03-26 14:02:22 +0100

    tests: Add tests/cursor.spec.ts

---
+++ tests/cursor.spec.ts
@@ -0,0 +1,24 @@
+import { expect, type Page, test } from '@playwright/test'
+import { EditorName } from '../src/cdrt/types'
+import { loadPrototype } from './utils'
+
+test('Cursor of first editor is visible in second editor', async ({ page }) => {
+  await loadPrototype(page)
+
+  await clickInEditor(page, EditorName.Editor1)
+
+  await expectCursorInEditor(page, EditorName.Editor2)
+})
+
+function clickInEditor(page: Page, editorName: EditorName) {
+  return page
+    .getByLabel(editorName)
+    .getByText(/This is an example/)
+    .click()
+}
+
+function expectCursorInEditor(page: Page, editorName: EditorName) {
+  return expect(
+    page.getByLabel(editorName).locator('.ProseMirror-loro-cursor'),
+  ).toBeVisible()
+}
