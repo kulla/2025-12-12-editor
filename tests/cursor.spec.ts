b6e3c73769d085284a35e1cebfd0e5e93ebb4cfc
Stephan Kulla <git.mail@kulla.me>
2026-03-26 14:06:37 +0100

    tests: Add (failing) test for cursor bug
    See https://github.com/kulla/2025-12-12-editor/issues/23

--- tests/cursor.spec.ts
+++ tests/cursor.spec.ts
@@ -10,6 +10,15 @@ test('Cursor of first editor is visible in second editor', async ({ page }) => {
   await expectCursorInEditor(page, EditorName.Editor2)
 })
 
+test('Cursor changes when editor focus is changed', async ({ page }) => {
+  await loadPrototype(page)
+
+  await clickInEditor(page, EditorName.Editor2)
+  await clickInEditor(page, EditorName.Editor1)
+
+  await expectCursorInEditor(page, EditorName.Editor2)
+})
+
 function clickInEditor(page: Page, editorName: EditorName) {
   return page
     .getByLabel(editorName)
