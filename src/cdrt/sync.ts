2a4d491ec4ebb67d4efd18c9e702636bee232a86
Stephan Kulla <git.mail@kulla.me>
2026-03-26 13:21:48 +0100

    refactor(cdrt): Rename variables in syncCDRTs()

--- src/cdrt/sync.ts
+++ src/cdrt/sync.ts
@@ -9,30 +9,30 @@ export function syncCDRTs(cdrt1: CDRT, cdrt2: CDRT) {
   loro1.import(loro2.export({ mode: 'update' }))
   loro2.import(loro1.export({ mode: 'update' }))
 
-  const unsubscribeA = loro1.subscribeLocalUpdates((bytes) =>
+  const unsubscribe1 = loro1.subscribeLocalUpdates((bytes) =>
     loro2.import(bytes),
   )
-  const unsubscribeB = loro2.subscribeLocalUpdates((bytes) =>
+  const unsubscribe2 = loro2.subscribeLocalUpdates((bytes) =>
     loro1.import(bytes),
   )
 
-  const awarenessAListener: AwarenessListener = (_, origin) => {
+  const awareness1Listener: AwarenessListener = (_, origin) => {
     if (origin === 'local') {
       awareness2.apply(awareness1.encode([loro1.peerIdStr]))
     }
   }
-  const awarenessBListener: AwarenessListener = (_, origin) => {
+  const awareness2Listener: AwarenessListener = (_, origin) => {
     if (origin === 'local') {
       awareness1.apply(awareness2.encode([loro2.peerIdStr]))
     }
   }
-  awareness1.addListener(awarenessAListener)
-  awareness2.addListener(awarenessBListener)
+  awareness1.addListener(awareness1Listener)
+  awareness2.addListener(awareness2Listener)
 
   return () => {
-    unsubscribeA()
-    unsubscribeB()
-    awareness1.removeListener(awarenessAListener)
-    awareness2.removeListener(awarenessBListener)
+    unsubscribe1()
+    unsubscribe2()
+    awareness1.removeListener(awareness1Listener)
+    awareness2.removeListener(awareness2Listener)
   }
 }
