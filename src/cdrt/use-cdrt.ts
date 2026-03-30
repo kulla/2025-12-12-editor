1582dac6b35571795d36d66a2e342f9601799da2
Stephan Kulla <git.mail@kulla.me>
2026-03-30 18:32:04 +0200

    fix(cdrt): Use proper useMemo in useCDRT()

--- src/cdrt/use-cdrt.ts
+++ src/cdrt/use-cdrt.ts
@@ -1,11 +1,13 @@
 import { LoroDoc } from 'loro-crdt'
 import { CursorAwareness } from 'loro-prosemirror'
-import { useRef } from 'react'
+import { useMemo } from 'react'
 import type { CDRT, EditorName } from './types'
 
 export function useCDRT(name: EditorName, color: string): CDRT {
-  const doc = useRef(new LoroDoc()).current
-  const awareness = useRef(new CursorAwareness(doc.peerIdStr)).current
+  return useMemo(() => {
+    const doc = new LoroDoc()
+    const awareness = new CursorAwareness(doc.peerIdStr)
 
-  return { name, doc, awareness, color }
+    return { name, doc, awareness, color }
+  }, [name, color])
 }
